1d0a9bac6290ade510ea659f4278978a7d76d4e2
calabrese.amelia <calabrese.amelia@SEMS-259-36.SEMS259.LAB>
2020-03-04 14:56:16 -0800

    magic 8 ball (start that)

--- src/_02_rock_paper_scissors/RockPaperScissors.java
+++ src/_02_rock_paper_scissors/RockPaperScissors.java
@@ -81,7 +81,7 @@ public class RockPaperScissors extends JPanel implements ActionListener{
 	//   Don't forget to create a Random object.
 		Random rndm = new Random();
         //2. Change the value of opponentSelection to be a random number between 0 and 2; 
-        int opponentSelection = 0;
+        int opponentSelection = rndm.nextInt(3);
         
         //3. Run the program again. Is the result different?
 
--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -11,13 +11,30 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
-		
+for (int i = 0; i < 10; i++) {
 		int randomNumber = randomMaker.nextInt(5);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+			
+	
+if (randomNumber==0){
+	JOptionPane.showMessageDialog(null, "you be lookin good... sort of");
+}
+else if (randomNumber==1){
+	JOptionPane.showMessageDialog(null, "Something smells good... oh! THAT's YOU??????");
+}
+else if (randomNumber==2){
+	JOptionPane.showMessageDialog(null, "like yo ummmmm... personality?");
+}
+else if (randomNumber==3){
+	JOptionPane.showMessageDialog(null, "you look wierd I,I mean nice (stammer stammer)");
+}
+else if (randomNumber==4){
+	JOptionPane.showMessageDialog(null, "Your cats are cute but mine are cuter.");
+}
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
