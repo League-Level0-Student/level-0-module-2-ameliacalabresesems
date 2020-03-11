834f4a4e21ff61aa5fb42ddd73fa7f23121e2fa7
calabrese.amelia <calabrese.amelia@SEMS-259-36.SEMS259.LAB>
2020-03-11 14:57:20 -0700

    Still on lottery numbers.

--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -6,30 +6,28 @@ package _05_magic_8_ball;
 
 import java.util.Random;
 
-public class Magic8Ball {
+import javax.swing.JOptionPane;
 
 	// 1. Make a main method that includes all the steps below….
-
+	public class Magic8Ball {
+		public static void main(String[] args) {
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+Random rmrm = new Random ();
+int rndm= rmrm.nextInt(2);
 	// 3. Print out this variable
-
+System.out.println(rmrm);
 	// 4. Get the user to enter a question for the 8 ball
-
+String input= JOptionPane.showInputDialog(null, "Ask the magic 8 ball a yes or no question");
 	// 5. If the random number is 0
-
+if (rndm==0) {
 	// -- tell the user "Yes"
-
+	JOptionPane.showMessageDialog(null, "Yes");
+}
 	// 6. If the random number is 1
+else if (rndm==1) {
 
 	// -- tell the user "No"
-
-	// 7. If the random number is 2
-
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
-
+	JOptionPane.showMessageDialog(null, "No");
+}
+}
 }
---
+++ src/_06_lottery_numbers/LotteryNumbers.java
@@ -0,0 +1,98 @@
+package _06_lottery_numbers;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class LotteryNumbers {
+	public static void main(String[] args) {
+	
+		Random eno = new Random ();
+		int one= eno.nextInt(9);
+
+		int two= eno.nextInt(9);
+		
+		int three= eno.nextInt(9);
+		
+		int four= eno.nextInt(9);
+		
+		int five= eno.nextInt(9);
+		
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+		
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+
+if (one==1) {
+	JOptionPane.showMessageDialog(null,"1");
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	}
+}
